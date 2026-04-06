ce8adb59718627612b818fc7691804fd82bc7d0e
Stimpy <95110502+StimpyDev@users.noreply.github.com>
2026-04-06 11:40:34 +0200

    Refactor RTPCommand to improve command execution

--- common/src/main/java/com/gmail/picono435/randomtp/commands/RTPCommand.java
+++ common/src/main/java/com/gmail/picono435/randomtp/commands/RTPCommand.java
@@ -28,44 +28,46 @@ public class RTPCommand {
 	}
 	
 	private static int runCommand(ServerPlayer p) {
-		try {
-			if(!RandomTPAPI.checkCooldown(p, cooldowns) && !RandomTPAPI.hasPermission(p, "randomtp.cooldown.exempt")) {
-				long secondsLeft = RandomTPAPI.getCooldownLeft(p, cooldowns);
-				Component cooldownmes = Component.literal(Messages.getCooldown().replaceAll("\\{secondsLeft\\}", Long.toString(secondsLeft)).replaceAll("\\{playerName\\}", p.getName().getString()).replaceAll("&", "§"));
-				p.sendSystemMessage(cooldownmes, false);
-				return 1;
-			} else {
-				cooldowns.remove(p.getName().getString());
-				if(Config.useOriginal()) {
-					Component finding = Component.literal(Messages.getFinding().replaceAll("\\{playerName\\}", p.getName().getString()).replaceAll("\\{blockX\\}", "" + (int)p.position().x).replaceAll("\\{blockY\\}", "" + (int)p.position().y).replaceAll("\\{blockZ\\}", "" + (int)p.position().z).replaceAll("&", "§"));
-					p.sendSystemMessage(finding, false);
-					if(!Config.getDefaultWorld().equals("playerworld")) {
-						RandomTPAPI.randomTeleport(p, RandomTPAPI.getWorld(Config.getDefaultWorld(), p.level().getServer()));
-					} else {
-						RandomTPAPI.randomTeleport(p, p.level());
-					}
-					cooldowns.put(p.getName().getString(), System.currentTimeMillis());
-					return 1;
-				}
-				double cal = p.level().getWorldBorder().getSize()/2;
-				BigDecimal num = new BigDecimal(cal);
-				String maxDistance = num.toPlainString();
-				if(Config.getMaxDistance() == 0) {
-					String command = "spreadplayers " + p.level().getWorldBorder().getCenterX() + " " + p.level().getWorldBorder().getCenterZ() + " " + Config.getMinDistance() + " " + maxDistance + " false " + p.getName().getString().toLowerCase();
-					p.level().getServer().getCommands().performCommand(p.level().getServer().getCommands().getDispatcher().parse(command, p.level().getServer().createCommandSourceStack()), command);
-					Component successful = Component.literal(Messages.getSuccessful().replaceAll("\\{playerName\\}", p.getName().getString()).replaceAll("\\{blockX\\}", "" + (int)p.position().x).replaceAll("\\{blockY\\}", "" + (int)p.position().y).replaceAll("\\{blockZ\\}", "" + (int)p.position().z).replaceAll("&", "§"));
-					p.sendSystemMessage(successful, false);
-				} else {
-					String command = "spreadplayers " + p.level().getWorldBorder().getCenterX() + " " + p.level().getWorldBorder().getCenterZ() + " " + Config.getMinDistance() + " " + Config.getMaxDistance() + " false " + p.getName().getString().toLowerCase();
-					p.level().getServer().getCommands().performCommand(p.level().getServer().getCommands().getDispatcher().parse(command, p.level().getServer().createCommandSourceStack()), command);
-					Component successful = Component.literal(Messages.getSuccessful().replaceAll("\\{playerName\\}", p.getName().getString()).replaceAll("\\{blockX\\}", "" + (int)p.position().x).replaceAll("\\{blockY\\}", "" + (int)p.position().y).replaceAll("\\{blockZ\\}", "" + (int)p.position().z).replaceAll("&", "§"));
-					p.sendSystemMessage(successful, false);
-				}
-				cooldowns.put(p.getName().getString(), System.currentTimeMillis());
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return 1;
-	}
+    try {
+        if(!RandomTPAPI.checkCooldown(p, cooldowns) && !RandomTPAPI.hasPermission(p, "randomtp.cooldown.exempt")) {
+            long secondsLeft = RandomTPAPI.getCooldownLeft(p, cooldowns);
+            Component cooldownmes = Component.literal(Messages.getCooldown().replaceAll("\\{secondsLeft\\}", Long.toString(secondsLeft)).replaceAll("\\{playerName\\}", p.getName().getString()).replaceAll("&", "§"));
+            p.sendSystemMessage(cooldownmes, false);
+            return 1;
+        } else {
+            cooldowns.remove(p.getName().getString());
+            if(Config.useOriginal()) {
+                Component finding = Component.literal(Messages.getFinding().replaceAll("\\{playerName\\}", p.getName().getString()).replaceAll("\\{blockX\\}", "" + (int)p.position().x).replaceAll("\\{blockY\\}", "" + (int)p.position().y).replaceAll("\\{blockZ\\}", "" + (int)p.position().z).replaceAll("&", "§"));
+                p.sendSystemMessage(finding, false);
+                if(!Config.getDefaultWorld().equals("playerworld")) {
+                    RandomTPAPI.randomTeleport(p, RandomTPAPI.getWorld(Config.getDefaultWorld(), p.getServer()));
+                } else {
+                    RandomTPAPI.randomTeleport(p, p.serverLevel());
+                }
+                cooldowns.put(p.getName().getString(), System.currentTimeMillis());
+                return 1;
+            }
+
+            double cal = p.serverLevel().getWorldBorder().getSize()/2;
+            BigDecimal num = new BigDecimal(cal);
+            String maxDistance = num.toPlainString();
+            
+            CommandSourceStack silentSource = p.getServer().createCommandSourceStack()
+                .withPermission(4)
+                .withSuppressedOutput();
+
+            String distance = (Config.getMaxDistance() == 0) ? maxDistance : String.valueOf(Config.getMaxDistance());
+            String command = "spreadplayers " + p.serverLevel().getWorldBorder().getCenterX() + " " + p.serverLevel().getWorldBorder().getCenterZ() + " " + Config.getMinDistance() + " " + distance + " false " + p.getScoreboardName();
+
+            p.getServer().getCommands().performCommand(silentSource, command);
+
+            Component successful = Component.literal(Messages.getSuccessful().replaceAll("\\{playerName\\}", p.getName().getString()).replaceAll("\\{blockX\\}", "" + (int)p.position().x).replaceAll("\\{blockY\\}", "" + (int)p.position().y).replaceAll("\\{blockZ\\}", "" + (int)p.position().z).replaceAll("&", "§"));
+            p.sendSystemMessage(successful, false);
+            
+            cooldowns.put(p.getName().getString(), System.currentTimeMillis());
+        }
+    } catch (Exception ex) {
+        ex.printStackTrace();
+    }
+    return 1;
 }
